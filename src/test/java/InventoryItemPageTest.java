72ffdb0b2a13363ef71f8ec5e73f2f7f1e42a93b
Erdős Marcell <erdosmarci@gmail.com>
2024-09-17 11:09:18 +0200

    extend InventoryItemTests

--- src/test/java/InventoryItemPageTest.java
+++ src/test/java/InventoryItemPageTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InventoryItemPageTest extends TestBase{
@@ -14,5 +15,27 @@ public class InventoryItemPageTest extends TestBase{
         inventoryPage.showProductDetails("Sauce Labs Backpack");
         inventoryItemPage.addToCart();
         assertTrue(inventoryItemPage.isProductInCart());
+    }
+    @Test
+    public void testRemoveItemFromInventoryFromItemPage() {
+        inventoryPage.showProductDetails("Sauce Labs Backpack");
+        inventoryItemPage.addToCart();
+        inventoryItemPage.removeFromCart();
+        assertFalse(inventoryItemPage.isProductInCart());
+    }
+    @Test
+    public void testAddItemToCartFromInventoryPageAndRemoveFromItemPage() {
+        inventoryPage.addProductToCart("Sauce Labs Backpack");
+        inventoryPage.showProductDetails("Sauce Labs Backpack");
+        inventoryItemPage.removeFromCart();
+        assertFalse(inventoryItemPage.isProductInCart());
+    }
+    @Test
+    public void testAddItemToCartFromItemPageAndRemoveFromInventoryPage() {
+        inventoryPage.showProductDetails("Sauce Labs Backpack");
+        inventoryItemPage.addToCart();
+        inventoryItemPage.backToProductsPage();
+        inventoryPage.removeProductFromCart("Sauce Labs Backpack");
+        assertFalse(inventoryItemPage.isProductInCart());
     }
 }
